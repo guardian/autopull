47dadb9d1f16fc1523160a2bc91b5bd5436eba6f
Andy Gallagher <andy.gallagher@theguardian.com>
2020-04-01 14:40:18 +0100

    debug

--- ci_scripts/build.sh
+++ ci_scripts/build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+echo Building for Windows...
 GOOS=windows GOARCH=amd64 go build -o autopull.exe
+ls -lh autopull.exe
+echo Building for Mac...
 GOOS=darwin GOARCH=amd64 go build -o autopull.macos
+ls -lh autopull.macos
+echo Bulding for Linux...
 GOOS=linux GOARCH=amd64 go build -o autopull.linux64
+ls -lh autopull.linux64
