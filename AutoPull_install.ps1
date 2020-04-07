41288453808a98d01bbbfd92df140b82c53167f3
richardholst <richardholst@outlook.com>
2020-04-07 11:27:29 +0100

    removed install_handler.ps1 script and added the code to the AutoPull_Install.ps1
    Updated the AutoPull_Install.ps1 to force overwite of files if they already on the sytem - in the case of reinstalll

--- AutoPull_install.ps1
+++ AutoPull_install.ps1
@@ -1,11 +1,18 @@
 # make dir  
-mkdir "C:\Program Files (x86)\autopull\"
+mkdir "C:\Program Files (x86)\autopull\" -force
 # Download Software
 Invoke-WebRequest -Uri "http://andydeliverablesbucket.s3.eu-west-2.amazonaws.com/autopull-win.zip" -OutFile "C:\Program Files (x86)\autopull\autopull-win.zip"
 # Unzip
-Expand-Archive -Path "C:\Program Files (x86)\autopull\autopull-win.zip" -DestinationPath "C:\Program Files (x86)\autopull"
+Expand-Archive -Path "C:\Program Files (x86)\autopull\autopull-win.zip" -DestinationPath "C:\Program Files (x86)\autopull" -force
 # set Execution policy
 Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
 # set download location for currently logged on user
 (Get-Content "C:\Program Files (x86)\autopull\autopull\autopull.yaml").Replace("download_path:","download_path: $env:USERPROFILE\downloads")|
 Set-Content "C:\Program Files (x86)\autopull\autopull\autopull.yaml"
+
+# register the protocol handler
+New-Item -Path registry::HKCR\ -Name archivehunter -Value "URL:archivehunter Protocol"
+New-ItemProperty -Path registry::HKCR\archivehunter -Name "URL Protocol" -Type string
+New-Item -Path registry::HKCR\archivehunter -Name "shell"
+New-Item -Path registry::HKCR\archivehunter\shell -Name "open"
+New-Item -Path registry::HKCR\archivehunter\shell\open -Name "command" -Value "`"c:\Program Files (x86)\autopull\autopull\autopull.exe`" %1"
